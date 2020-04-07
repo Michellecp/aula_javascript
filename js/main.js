c3d14eb64ada8d297fd6d7101c02778e44899d14
Michellecp <chelle.info@gmail.com>
2020-04-06 21:53:35 -0300

    Atualização dos itens da aula

---
+++ js/main.js
@@ -0,0 +1,43 @@
+function clicou()
+{
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigado por clicar!!!!</b>";
+    //console.log(document.getElementById("agradecimento"));
+    //alert("Obrigada por clicar");
+}
+
+function redirecionar()
+{
+    window.open("https://web.digitalinnovation.one/track/code-fullstack-girl"); //Outra Aba
+   //window.location.href = "https://web.digitalinnovation.one/track/code-fullstack-girl"; //Na mesma aba
+}
+
+
+/*var idade = prompt("Qual a sua idade: ");
+if(idade <= 18){
+    alert("Menor de idade");
+}else{
+    alert("Maior de idade")
+}
+
+var frutas = [{nome: "maça", cor:"vermelha"}, {nome:"Uva", cor:"roxa"}]
+console.log(frutas);
+
+var fruta = {nome: "maça", cor:"vermelha"}
+console.log(fruta);
+
+var lista = ["maça", "pera", "laranja"];
+lista.push("uva");
+//lista.pop();
+console.log(lista);
+console.log(lista.toString());
+console.log(lista.join(" | "));
+
+var nome = "Michelle Pacheco";
+var n1 = 1;
+var n2 = 3;
+var frase = "Japão é o melhor time do mundo";
+//alert(nome + " tem " + idade + " anos ");
+
+console.log(nome);
+console.log(n1+n2);
+console.log(frase.toLowerCase());*/
